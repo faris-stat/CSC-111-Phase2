8092affade7947458cee9f556a0d8b6b07ed70c4
faris-stat <445102435@student.ksu.edu.sa>
2025-12-04 13:29:16 +0300

    Create Ff.java

---
+++ Ff.java
@@ -0,0 +1,22 @@
+public class Ff{
+private int id;
+private String name;
+private int age;
+private double gpa;
+public Ff(){
+name="NA";
+id=123456;
+age=20;
+gpa=1;
+}
+public Ff(int id,String name,int age,double gpa){
+setid(id);
+setname(name);
+setage(age);
+setgpa(gpa);
+}
+public void setid(int id){
+}
+public int getid(){
+return id;
+}
